7e1803db26d937ed3431ce307b5e7030c2899a1e
MantsSk <mantas.skara@gmail.com>
2023-03-22 11:29:06 +0200

    more example

--- Chapter03 - IndentationCommentsVariables/variable_example.py
+++ Chapter03 - IndentationCommentsVariables/variable_example.py
@@ -3,3 +3,9 @@ my_name = "CodingArchy"
 print("My name is " + my_name)
 print("I would like to be called " + my_name)
 print("My friends call me CodingArchy" + my_name)
+
+my_number = 4
+
+print("My name is " + str(my_number))
+print("I would like to be called " + str(my_number))
+print("My friends call me CodingArchy" + str(my_number))
